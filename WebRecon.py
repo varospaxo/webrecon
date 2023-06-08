20e352cffdc0b8ba352cd0073b83c58995264b2a
Vedant Farkade <64273563+varospaxo@users.noreply.github.com>
2023-06-09 03:55:11 +0530

    Added Domain Name Regex

--- WebRecon.py
+++ WebRecon.py
@@ -2,6 +2,7 @@ import time
 import os
 import subprocess
 import shutil
+import re
 
 if os.path.exists("Temp"):
   shutil.rmtree("Temp", ignore_errors=True)
@@ -16,17 +17,41 @@ print("| |/ |/ /  __/ /_/ / _, _/  __/ /__/ /_/ / / / /")
 print("|__/|__/\___/_.___/_/ |_|\___/\___/\____/_/ /_/ ")
 print("                                                ")
 hostname = input("Enter the hostname: ")
-file = open("./Result.txt", "a")         
-file.write(hostname)
-t = time.localtime()
-current_time = time.strftime("%m-%d-%Y_%H:%M:%S", t)
-file.write ("\n"+str(current_time))
-print("Current Time: ",current_time)
-file.close()
+def isValidDomain(str):
+ 
+    # Regex to check valid
+    # domain name.
+    regex = "^((?!-)[A-Za-z0-9-]" + "{1,63}(?<!-)\\.)" +"+[A-Za-z]{2,6}"
+     
+    # Compile the ReGex
+    p = re.compile(regex)
+ 
+    # If the string is empty
+    # return false
+    if (str == None):
+        return False
+ 
+    # Return if the string
+    # matched the ReGex
+    if(re.search(p, str)):
+        return True
+    else:
+        return False
 
+while isValidDomain(hostname) == False:
+    print("Invalid domain name format!!!\n")
+    hostname = input("Re-enter the hostname: ")
+else:
+    file = open("./Result.txt", "a")         
+    file.write(hostname)
+    t = time.localtime()
+    current_time = time.strftime("%m-%d-%Y_%H:%M:%S", t)
+    file.write ("\n"+str(current_time))
+    print("Current Time: ",current_time)
+    file.close()
 
 if os.path.exists("Temp"):
-    pass
+    shutil.rmtree("Temp", ignore_errors=True)
 else:
     os.mkdir("Temp")
     
