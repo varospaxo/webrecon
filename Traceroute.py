6addd489ad661119d24a8236dac9a664026aa1d1
Vedant Farkade <64273563+varospaxo@users.noreply.github.com>
2023-11-05 23:09:42 +0530

    Updated Traceroute

--- Traceroute.py
+++ Traceroute.py
@@ -3,35 +3,41 @@ import scapy.all as scapy
 import os
 import subprocess
 
+try:
+    print("\n-----------Traceroute Scan Started-----------\n")
+    
+    with open('./Temp/Result_current.txt') as f:
+        hostname = f.readline().strip()
+        currenttime = f.readline().strip()
+        target = f.readline().strip()
+        print(target)
+    
+    if len(sys.argv) == 2:
+        target = sys.argv[1]
 
-print(f"\n-----------Traceroute Scan Started-----------\n")
-with open('./Temp/Result_current.txt') as f:
-      hostname = f.readline().strip()
-      currenttime = f.readline().strip()
-      target = f.readline().strip()
-      print (target)
-if len(sys.argv) == 2:
-    target = sys.argv[1]
+    ans, unans = scapy.sr(scapy.IP(dst=target, ttl=(1, 22), id=scapy.RandShort()) / scapy.TCP(flags=0x2), timeout=10)
 
-ans, unans = scapy.sr(scapy.IP(dst=target, ttl=(1,22),id=scapy.RandShort())/scapy.TCP(flags=0x2), timeout=10)
+    for snd, rcv in ans:
+        if isinstance(rcv.payload, scapy.TCP):
+            rambo = "(TCP)"
+        else:
+            rambo = ""
+        print(snd.ttl, rcv.src, rambo)
+        f = open("./Traceroute.txt", "a")
+        f.writelines(str(snd.ttl) + "." + " " + str(rcv.src) + " " + rambo)
+        f.writelines("\n")
+        f.close()
+    
+    path_current = "./Traceroute.txt"
+    movepath = "./Temp/Traceroute_OP.txt"
+    os.replace(path_current, movepath)
 
-for snd,rcv in ans:
-    if isinstance(rcv.payload, scapy.TCP):
-        rambo="(TCP)"
-    else:
-        rambo=""
-    print(snd.ttl, rcv.src, rambo)
-    f = open("./Traceroute.txt", "a")
-    f.writelines(str(snd.ttl)+"."+" " + str(rcv.src)+ " "+rambo)
-    f.writelines("\n")
-    f.close()
-path_current="./Traceroute.txt"
-movepath = "./Temp/Traceroute_OP.txt" 
-os.replace(path_current, movepath)
-
-print(f"\n-----Scanning Finished-----\n")
-
-#Run Next Script
-rawpath = os.getcwd() + "\\OSDetect.py"
+    print("\n-----Scanning Finished-----\n")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
+    # Run Next Script
+rawpath = os.getcwd() + "/OSDetect.py"
 path = rawpath.replace('\\', '/')
 subprocess.call(['python', path])
+
+
