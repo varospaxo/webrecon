bb0faf3ba28beae0ab40109969dba3023a64af42
Vedant Farkade <64273563+varospaxo@users.noreply.github.com>
2023-11-05 23:22:05 +0530

    Update Reporter.py

--- Reporter.py
+++ Reporter.py
@@ -25,96 +25,58 @@ pdf.cell(200, 2, txt = "", ln = 1, align = 'C')
 pdf.line(10, int(pdf.get_y()), 200, int(pdf.get_y()))
 pdf.cell(200, 1, txt = "", ln = 1, align = 'C')
 pdf.line(10, int(pdf.get_y()), 200, int(pdf.get_y()))
-# open the text file in read mode
-f = open("./Temp/Result_current.txt", "r")
-# insert the texts in pdf
-for x in f:
-    pdf.cell(200, 10, txt = x, ln = 1, align = 'C')
-pdf.line(10, int(pdf.get_y()), 200, int(pdf.get_y()))
-pdf.cell(200, 1, txt = "", ln = 1, align = 'C')
-pdf.line(10, int(pdf.get_y()), 200, int(pdf.get_y()))
-pdf.cell(200, 10, txt = "", ln = 1, align = 'C')
+try:
+    # Open the text file in read mode
+    f = open("./Temp/Result_current.txt", "r")
 
-# open the text file in read mode
-f = open("./Temp/Ping_OP.txt", "r")
-pdf.cell(200, 10, txt = "Host Service Detection", ln = 1, align = 'L')
-pdf.line(10, int(pdf.get_y()), 100, int(pdf.get_y()))
-# insert the texts in pdf
-for x in f:
-    pdf.cell(200, 10, txt = x, ln = 1, align = 'L')
-pdf.cell(200, 6, txt = "", ln = 1, align = 'C')
+    # Insert the texts in the PDF
+    for x in f:
+        pdf.cell(200, 10, txt=x, ln=1, align='C')
 
-# open the text file in read mode
-f = open("./Temp/PortScannerFast_OP.txt", "r")
-pdf.cell(200, 10, txt = "Port Scan", ln = 1, align = 'L')
-pdf.line(10, int(pdf.get_y()), 100, int(pdf.get_y()))
-# insert the texts in pdf
-for x in f:
-    pdf.cell(200, 10, txt = x, ln = 1, align = 'L')
-pdf.cell(200, 6, txt = "", ln = 1, align = 'C')
+    f.close()
+except FileNotFoundError:
+    print("Result_current.txt is not present. Skipping to the next file.")
 
-# open the text file in read mode
-f = open("./Temp/Traceroute_OP.txt", "r")
-pdf.cell(200, 10, txt = "Traceroute", ln = 1, align = 'L')
-pdf.line(10, int(pdf.get_y()), 100, int(pdf.get_y()))
-# insert the texts in pdf
-for x in f:
-    pdf.cell(200, 10, txt = x, ln = 1, align = 'L')
-pdf.cell(200, 6, txt = "", ln = 1, align = 'C')
+# Define a list of files to process
+file_paths = [
+    "./Temp/Ping_OP.txt",
+    "./Temp/PortScannerFast_OP.txt",
+    "./Temp/Traceroute_OP.txt",
+    "./Temp/OSDetect_OP.txt",
+    "./Temp/SSLCert_OP.txt",
+    "./Temp/ReverseDNS_OP.txt",
+    "./Temp/Subdomain_OP.txt",
+    "./Temp/AdminPages_OP.txt"
+]
 
-# open the text file in read mode
-f = open("./Temp/OSDetect_OP.txt", "r")
-pdf.cell(200, 10, txt = "Remote OS Detection", ln = 1, align = 'L')
-pdf.line(10, int(pdf.get_y()), 100, int(pdf.get_y()))
-# insert the texts in pdf
-for x in f:
-    pdf.cell(200, 10, txt = x, ln = 1, align = 'L')
-pdf.cell(200, 6, txt = "", ln = 1, align = 'C')
+for file_path in file_paths:
+    try:
+        # Open the text file in read mode
+        f = open(file_path, "r")
 
-# open the text file in read mode
-f = open("./Temp/SSLCert_OP.txt", "r")
-pdf.cell(200, 10, txt = "SSL Status", ln = 1, align = 'L')
-pdf.line(10, int(pdf.get_y()), 100, int(pdf.get_y()))
-# insert the texts in pdf
-for x in f:
-    pdf.cell(200, 10, txt = x, ln = 1, align = 'L')
-pdf.cell(200, 6, txt = "", ln = 1, align = 'C')
+        # Extract the filename from the file path
+        filename = os.path.basename(file_path)
 
-# open the text file in read mode
-f = open("./Temp/ReverseDNS_OP.txt", "r")
-pdf.cell(200, 10, txt = "Reverse DNS", ln = 1, align = 'L')
-pdf.line(10, int(pdf.get_y()), 100, int(pdf.get_y()))
-# insert the texts in pdf
-for x in f:
-    pdf.cell(200, 10, txt = x, ln = 1, align = 'L')
-pdf.cell(200, 6, txt = "", ln = 1, align = 'C')
+        # Insert the texts in the PDF
+        pdf.cell(200, 10, txt=filename, ln=1, align='L')
+        pdf.line(10, int(pdf.get_y()), 100, int(pdf.get_y()))
 
-# open the text file in read mode
-f = open("./Temp/Subdomain_OP.txt", "r")
-pdf.cell(200, 10, txt = "Subdomain List", ln = 1, align = 'L')
-pdf.line(10, int(pdf.get_y()), 100, int(pdf.get_y()))
-# insert the texts in pdf
-for x in f:
-    pdf.cell(200, 10, txt = x, ln = 1, align = 'L')
-pdf.cell(200, 6, txt = "", ln = 1, align = 'C')
+        for x in f:
+            pdf.cell(200, 10, txt=x, ln=1, align='L')
 
-# open the text file in read mode
-f = open("./Temp/AdminPages_OP.txt", "r")
-pdf.cell(200, 10, txt = "Admin Pages Found", ln = 1, align = 'L')
-pdf.line(10, int(pdf.get_y()), 100, int(pdf.get_y()))
-# insert the texts in pdf
-for x in f:
-    pdf.cell(200, 10, txt = x, ln = 1, align = 'L')
-pdf.cell(200, 6, txt = "", ln = 1, align = 'C')
+        pdf.cell(200, 6, txt="", ln=1, align='C')
 
-# save the pdf with name .pdf
-time_now  = datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S') 
-name = "WebRecon_"+time_now
+        f.close()
+    except FileNotFoundError:
+        print(f"{file_path} is not present. Skipping to the next file.")
+
+# Save the PDF with a name
+time_now = datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S')
+name = "WebRecon_" + time_now
 print(name)
-pdf.output("./OutputFiles/"+name+".pdf")
-path = "'"+str(os.getcwd())+"\OutputFiles\\"+name+".pdf'"
-print("Output report saved in """+colored(path, 'green'))
-
+pdf.output("./OutputFiles/" + name + ".pdf")
+path = os.path.join(os.getcwd(), "OutputFiles", name + ".pdf")
+print("Output report saved in " + colored(path, 'green'))
 
 # if os.path.exists("./Temp/Result_current.txt"):
 #   os.remove("./Temp/Result_current.txt")
